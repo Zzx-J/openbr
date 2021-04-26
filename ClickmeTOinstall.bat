5145eb295049680b90ada4f41cb03bbe38803488
Zzx-J <82952761+Zzx-J@users.noreply.github.com>
2021-04-26 17:20:16 +0800

    Update ClickmeTOinstall.bat

--- ClickmeTOinstall.bat
+++ ClickmeTOinstall.bat
@@ -1,30 +1,14 @@
 @echo.
-@echo ============================================
+@echo =============================================================
 @echo =   This is the installation program of OpenBr made by Zzx.
 @echo.
 @echo =   Please follow the instruction and wait patiently.
 @echo =   Remember to quit your antivirus program.
 @echo.
-@echo ============================================
+@echo =============================================================
 @echo.
 
-::cd ".\opencv"
-::mkdir build-msvc2019
-::cd build-msvc2019
-::cmake -G "NMake Makefiles" -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DWITH_FFMPEG=OFF -DCMAKE_BUILD_TYPE=Debug ../sources
-::nmake
-::nmake install
-::cmake -DCMAKE_BUILD_TYPE=Release ../sources
-::nmake
-::nmake install
-::nmake clean
 
-::pause
-
-
-
-::cd "..\..\openbr-master"
-::cd ".\openbr-master"
 mkdir build-msvc2019
 cd build-msvc2019
 cmake -G "CodeBlocks - NMake Makefiles" -DCMAKE_PREFIX_PATH="./opencv/build-msvc2019/install; ./msvc2019" -DCMAKE_INSTALL_PREFIX="./install" -DBR_INSTALL_DEPENDENCIES=ON -DCMAKE_BUILD_TYPE=Release ..
@@ -38,27 +22,29 @@ nmake
 nmake install
 
 @echo.
-@echo ****************************************************************************************************
+@echo ============================================================
+@echo ============================================================
+@echo =		The build of OpenBR is now finished.
 @echo.
-@echo *** Please open "openbr-master/build-msvc2019/openbr/cmake_install.cmake" and replace the code
-@echo ***            "/openbr-master/openbr/NOTFOUND/../bin/"
-@echo ***   with
-@echo ***            "/opencv/build-msvc2019/install/x86/vc16/bin/"
+@echo =   	You can now hack OpenBR.
+@echo =   	To begin with, open your qtcreator.exe
 @echo.
-@echo ****************************************************************************************************
+@echo =   	From the Qt Creator "Tools" menu select "Options..."
+@echo =   	Under "Kits" select "Desktop (default)"
+@echo =   	For "Compiler:" select "Microsoft Visual C++ Compiler 11.0 (x86_amd64)" and click "OK"
+@echo =   	From the Qt Creator "File" menu select "Open File or Project...".
+@echo =   	Select "\OpenBr_install\openbr-master\CMakeLists.txt" then "Open".
+@echo =   	Browse to your pre-existing build directory "\OpenBr_install\openbr-master\build-msvc2019" then select "Next".
+@echo =   	Select "Run CMake" then "Finish".
+@echo =   	You're all set! 
 @echo.
+@echo ===========================================================
+@echo ===========================================================
+@echo.
+@echo.
+@echo.
+@echo 		Press to quit.
 pause
-nmake install
-
-::F:\QT\Tools\QtCreator\bin\qtcreator.exe
-
-::cd C:\test\openbr-master\build-msvc2019
-::c:
-::cpack -G ZIP
-::pause
-::C:/test/opencv/build-msvc2019/install/x86/vc16/bin
-::replace
-::C:/test/openbr-master/openbr/NOTFOUND/../bin/
 
 
 
